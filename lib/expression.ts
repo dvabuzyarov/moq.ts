f0be38dd6660c6d2e5f2f36ac2b5699b3dd6a969
Dmitry Abuzyarov <dvabuzyarov@gmail.com>
2016-11-06 17:59:15 +0200

    The expression has been re-factored

--- lib/expression.ts
+++ lib/expression.ts
@@ -5,41 +5,26 @@
 export interface IExpressionInfo {
     name: string;
     arguments: any[];
-    next: IExpressionInfo;
 }
 
-function expressionProxy(info: IExpressionInfo, previous?: IExpressionInfo): any {
-    const next: IExpressionInfo = {name: undefined, arguments: undefined, next: undefined};
-
-    const setNext = ()=>{
-        if (previous !== undefined)
-            previous.next = info;
-    };
+function expressionProxy(info: IExpressionInfo): any {
 
     const options = {
         get(target, name) {
-            setNext();
             info.name = name;
-            return expressionProxy(next, info);
+            return (...args)=> info.arguments = args
         },
 
         apply(target, thisArg, args){
-            if (previous !== undefined && previous.name !== undefined && previous.arguments === undefined){
-                previous.arguments = args;
-                return expressionProxy(next, previous);
-            } else {
-                setNext();
-                info.arguments = args;
-                return expressionProxy(next, info);
-            }
+            info.arguments = args;
         }
     };
 
-    return new Proxy(function () {}, options);
+    return new Proxy(function () {  }, options);
 }
 
 export function expression<T, TResult>(expression: IExpression<T, TResult>): IExpressionInfo {
-    const info: IExpressionInfo = {name: undefined, arguments: undefined, next: undefined};
+    const info: IExpressionInfo = {name: undefined, arguments: undefined};
 
     const proxy = expressionProxy(info);
     expression(proxy);
--- tests/expression.UnitTests.ts
+++ tests/expression.UnitTests.ts
@@ -2,12 +2,20 @@
 
 describe('Expression', () => {
 
+    it('Resolves undefined expression', ()=> {
+        const actual = expression(instance => undefined);
+        const expected: IExpressionInfo = {
+            name: undefined,
+            arguments: undefined
+        };
+        expect(actual).toEqual(expected);
+    });
+
     it('Resolves empty', ()=> {
         const actual = expression(instance => instance);
         const expected: IExpressionInfo = {
             name: undefined,
-            arguments: undefined,
-            next: undefined
+            arguments: undefined
         };
         expect(actual).toEqual(expected);
     });
@@ -18,7 +26,6 @@ describe('Expression', () => {
         const expected: IExpressionInfo = {
             name: undefined,
             arguments: [arg],
-            next: undefined
         };
         expect(actual).toEqual(expected);
     });
@@ -28,7 +35,6 @@ describe('Expression', () => {
         const expected: IExpressionInfo = {
             name: 'member',
             arguments: undefined,
-            next: undefined
         };
         expect(actual).toEqual(expected);
     });
@@ -39,73 +45,6 @@ describe('Expression', () => {
         const expected: IExpressionInfo = {
             name: 'member',
             arguments: [arg],
-            next: undefined
-        };
-        expect(actual).toEqual(expected);
-    });
-
-    it('Resolves complex name', ()=> {
-        const actual = expression(instance => instance.member1.member2);
-        const expected: IExpressionInfo = {
-            name: 'member1',
-            arguments: undefined,
-            next: {
-                name: 'member2',
-                arguments: undefined,
-                next: undefined
-            }
-        };
-        expect(actual).toEqual(expected);
-    });
-
-    it('Resolves complex name and argument', ()=> {
-        const arg = 'argument';
-        const actual = expression(instance => instance.member1.member2(arg));
-        const expected: IExpressionInfo = {
-            name: 'member1',
-            arguments: undefined,
-            next: {
-                name: 'member2',
-                arguments: [arg],
-                next: undefined
-            }
-        };
-        expect(actual).toEqual(expected);
-    });
-
-    it('Resolves complex name with arguments and argument', ()=> {
-        const arg1 = 'argument 1';
-        const arg2 = 'argument 2';
-        const actual = expression(instance => instance.member1(arg1).member2(arg2));
-        const expected: IExpressionInfo = {
-            name: 'member1',
-            arguments: [arg1],
-            next: {
-                name: 'member2',
-                arguments: [arg2],
-                next: undefined
-            }
-        };
-        expect(actual).toEqual(expected);
-    });
-
-    it('Resolves complex name with arguments, function and name with argument', ()=> {
-        const arg1 = 'argument 1';
-        const arg2 = 'argument 2';
-        const arg3 = 'argument 3';
-        const actual = expression(instance => instance.member1(arg1)(arg2).member3(arg3));
-        const expected: IExpressionInfo = {
-            name: 'member1',
-            arguments: [arg1],
-            next: {
-                name: undefined,
-                arguments: [arg2],
-                next: {
-                    name: 'member3',
-                    arguments: [arg3],
-                    next: undefined
-                }
-            }
         };
         expect(actual).toEqual(expected);
     });
