374adba5ce38cd3f2eb9013a86b451fddb6c9348
Dmitry Abuzyarov <dvabuzyarov@gmail.com>
2016-11-15 22:58:41 +0200

    The named method expression matcher has been implemented.

---
+++ lib/named.method.expression-matcher.ts
@@ -0,0 +1,20 @@
+﻿import {It} from './expression-predicates';
+import {NamedMethodInfo} from './expression-reflector';
+import {IArgumentsMatcher} from './arguments-matcher';
+
+export class NamedMethodExpressionMatcher{
+
+    constructor(private argumentsMatcher: IArgumentsMatcher){
+
+    }
+
+    public matched(left: NamedMethodInfo, right: NamedMethodInfo|It<any>): boolean{
+        if (left === right) return true;
+        if (right instanceof It)
+            return (right as It<any>).invoke(left);
+
+        const rightExpression = right as NamedMethodInfo;
+        const argumentsAreMatched = this.argumentsMatcher.matched(left.arguments, rightExpression.arguments);
+        return left.name === rightExpression.name && argumentsAreMatched;
+    }
+}
---
+++ tests/named.method.expression-matcher.UnitTests.ts
@@ -0,0 +1,109 @@
+﻿import {It} from '../lib/expression-predicates';
+import {NamedMethodInfo} from '../lib/expression-reflector';
+import {IArgumentsMatcher} from '../lib/arguments-matcher';
+import {NamedMethodExpressionMatcher} from '../lib/named.method.expression-matcher';
+
+describe('Named method expression matcher', () => {
+
+    function ArgumentsMatcherFactory(matched?: (left: any[], right: (any|It<any>)[])=> boolean): IArgumentsMatcher {
+        return {
+            matched: matched
+        }
+    }
+
+    it('Returns true when both are the same object', ()=> {
+        const expressionInfo = new NamedMethodInfo('name', []);
+
+        const matcher = new NamedMethodExpressionMatcher(ArgumentsMatcherFactory(undefined));
+        const actual = matcher.matched(expressionInfo, expressionInfo);
+
+        expect(actual).toBe(true);
+    });
+
+
+    it('Returns true when they are equal', ()=> {
+        const arguments1 = [];
+        const arguments2 = [];
+
+        const name = 'name';
+        const left = new NamedMethodInfo(name, arguments1);
+        const right = new NamedMethodInfo(name, arguments2);
+
+        const matched = (lvalue, rvalue): boolean =>{
+            expect(lvalue).toBe(arguments1);
+            expect(rvalue).toBe(arguments2);
+            return true;
+        };
+
+        const matcher = new NamedMethodExpressionMatcher(ArgumentsMatcherFactory(matched));
+        const actual = matcher.matched(left, right);
+
+        expect(actual).toBe(true);
+    });
+
+    it('Returns true when right is predicate that returns true', ()=> {
+        const left = new NamedMethodInfo('name', []);
+
+        const right = It.Is((value)=> {
+            expect(value).toBe(left);
+            return true;
+        });
+
+        const matcher = new NamedMethodExpressionMatcher(ArgumentsMatcherFactory(undefined));
+        const actual = matcher.matched(left, right);
+
+        expect(actual).toBe(true);
+    });
+
+    it('Returns false when left does not equal to right by arguments', ()=> {
+        const arguments1 = [];
+        const arguments2 = [];
+
+        const name = 'name';
+        const left = new NamedMethodInfo(name, arguments1);
+        const right = new NamedMethodInfo(name, arguments2);
+
+        const matched = (lvalue, rvalue): boolean =>{
+            expect(lvalue).toBe(arguments1);
+            expect(rvalue).toBe(arguments2);
+            return false;
+        };
+
+        const matcher = new NamedMethodExpressionMatcher(ArgumentsMatcherFactory(matched));
+        const actual = matcher.matched(left, right);
+
+        expect(actual).toBe(false);
+    });
+
+    it('Returns false when left does not equal to right by name', ()=> {
+        const args = [];
+
+        const left = new NamedMethodInfo('left name', args);
+        const right = new NamedMethodInfo('right name', args);
+
+        const matched = (lvalue, rvalue): boolean =>{
+            expect(lvalue).toBe(args);
+            expect(rvalue).toBe(args);
+            return true;
+        };
+
+        const matcher = new NamedMethodExpressionMatcher(ArgumentsMatcherFactory(matched));
+        const actual = matcher.matched(left, right);
+
+        expect(actual).toBe(false);
+    });
+
+    it('Returns false when right is predicate that returns false', ()=> {
+        const left = new NamedMethodInfo('name', []);
+        const right = It.Is((value)=> {
+            expect(value).toBe(left);
+            return false;
+        });
+
+        const matcher = new NamedMethodExpressionMatcher(ArgumentsMatcherFactory(undefined));
+        const actual = matcher.matched(left, right);
+
+        expect(actual).toBe(false);
+    });
+
+});
