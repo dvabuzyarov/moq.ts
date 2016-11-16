e7ecfea6e7a1bcf3ef2d39faf97724be06566f85
Dmitry Abuzyarov <dvabuzyarov@gmail.com>
2016-11-17 00:17:42 +0200

    Expected expressions has been implemented.

--- lib/expected-expressions/expected-expressions.ts
+++ lib/expected-expressions/expected-expressions.ts
@@ -1,3 +1,20 @@
-/**
- * Created by dvabuzyarov on 11/16/16.
- */
+import {It} from './expression-predicates';
+import {SetPropertyExpression, GetPropertyExpression, MethodExpression, NamedMethodExpression} from '../expressions';
+
+export class ExpectedNamedMethodExpression extends NamedMethodExpression {
+
+}
+
+export class ExpectedMethodExpression extends MethodExpression {
+
+}
+
+export class ExpectedGetPropertyExpression extends GetPropertyExpression {
+
+}
+
+export class ExpectedSetPropertyExpression extends SetPropertyExpression {
+
+}
+
+export type ExpectedExpressions<T> = ExpectedMethodExpression | ExpectedGetPropertyExpression | ExpectedSetPropertyExpression | ExpectedNamedMethodExpression | It<T>;
