f0673b9c15e21f9706017dc62af0a50a0229105c
Dmitry Abuzyarov <dvabuzyarov@gmail.com>
2016-11-19 22:48:25 +0200

    update travis-ci config. The Karma config has been updated to use Firefox.

--- karma.conf.js
+++ karma.conf.js
@@ -65,8 +65,8 @@ module.exports = function (config) {
         
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        // browsers: ['FirefoxDeveloper'],
-        browsers: ['Chrome'],
+        browsers: ['FirefoxDeveloper'],
+        // browsers: ['Chrome'],
         
         
         // Continuous Integration mode
