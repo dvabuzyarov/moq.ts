54c4592e1e972da3c62f033a173b2d92378676ef
Dmitry Abuzyarov <dvabuzyarov@gmail.com>
2016-11-19 23:27:57 +0200

    npm deployment has been introduced

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['systemjs', 'jasmine', 'jasmine-matchers'],
-        plugins: ['karma-systemjs', 'karma-jasmine', 'karma-jasmine-matchers', 'karma-firefox-launcher', 'karma-chrome-launcher'],
+        plugins: ['karma-systemjs', 'karma-jasmine', 'karma-jasmine-matchers', 'karma-firefox-launcher'],
         systemjs: {
             // Path to your SystemJS configuration file
             configFile: './system.conf.js',
