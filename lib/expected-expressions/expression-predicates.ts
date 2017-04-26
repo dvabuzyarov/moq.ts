3794a48c85b515924bd72e60e068d4f597735adb
Dmitry Abuzyarov <dvabuzyarov@gmail.com>
2017-04-26 21:05:50 +0300

    It predicate has been improved.

--- lib/expected-expressions/expression-predicates.ts
+++ lib/expected-expressions/expression-predicates.ts
@@ -4,19 +4,24 @@
 
 
 export class It<T> {
-    constructor(public predicate: IPredicate<T>){
+    constructor(public predicate: IPredicate<T>) {
 
     }
 
-    public static Is<T>(predicate: IPredicate<T>): It<T>|any {
+    public static Is<T>(predicate: IPredicate<T>): It<T> | any {
         return new It(predicate);
     }
 
-    public static IsAny<T>(): It<T>|any {
-        return new It(()=>true);
+    public static IsAny<T>(): It<T> | any {
+        return new It(() => true);
     }
 
-    public test(instance: T): boolean{
-        return this.predicate(instance);
+    public test(instance: T): boolean {
+        try {
+            const result = this.predicate(instance);
+            return result === true || result === undefined;
+        } catch (e) {
+            return false;
+        }
     }
 }
--- tests.unit/expected-expressions/expression-predicates.UnitTests.ts
+++ tests.unit/expected-expressions/expression-predicates.UnitTests.ts
@@ -21,4 +21,48 @@ describe('Expression Predicates', () => {
         expect(actual).toBe(true);
     });
 
+    it('Returns true when predicates returns true', ()=> {
+        const value = 'tested value';
+
+        const predicate = jasmine.createSpy('predicate').and.returnValue(true);
+        const it = new It(predicate);
+        const actual = it.test(value);
+
+        expect(actual).toBe(true);
+        expect(predicate).toHaveBeenCalledWith(value);
+    });
+
+    it('Returns true when predicates returns undefined', ()=> {
+        const value = 'tested value';
+
+        const predicate = jasmine.createSpy('predicate').and.returnValue(undefined);
+        const it = new It(predicate);
+        const actual = it.test(value);
+
+        expect(actual).toBe(true);
+        expect(predicate).toHaveBeenCalledWith(value);
+    });
+
+    it('Returns false when predicates returns anything that are not true or undefined', ()=> {
+        const value = 'tested value';
+
+        const predicate = jasmine.createSpy('predicate').and.returnValue(1);
+        const it = new It(predicate);
+        const actual = it.test(value);
+
+        expect(actual).toBe(false);
+        expect(predicate).toHaveBeenCalledWith(value);
+    });
+
+    it('Returns false when predicates throws an exception', ()=> {
+        const value = 'tested value';
+
+        const predicate = jasmine.createSpy('predicate').and.throwError('exception message');
+        const it = new It(predicate);
+        const actual = it.test(value);
+
+        expect(actual).toBe(false);
+        expect(predicate).toHaveBeenCalledWith(value);
+    });
+
 });
