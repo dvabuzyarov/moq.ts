09ead1610e5bdbb768dfbd17e568b7b3206af367
Documentation Builder <documentation@moq.ts>
2017-07-09 22:46:53 +0000

    Automated documentation build for changeset 17405b98b00ef60cf414fa9065d5f28106295c98.

--- lib/format-string.ts
+++
@@ -1,3 +0,0 @@
-export function format(template: string, ...args: any[]): string {
-    return template.replace(/\{(\d+)\}/g, (match, capture) => args[1 * capture]);
-}
--- lib/formatters/get.property-formatter.ts
+++
@@ -1,8 +0,0 @@
-import {GetPropertyExpression} from '../expressions';
-
-export class GetPropertyExpressionFormatter{
-
-    public format(expression: GetPropertyExpression): string{
-        return `Getter of \'${expression.name}\'`;
-    }
-}
