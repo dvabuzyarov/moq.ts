750da5855ff9697180a5e0d947d28b3a5d658b13
dvabuzyarov <dvabuzyarov@gmail.com>
2019-02-04 23:47:20 +0200

    refactor(explorer): implement preset has method explorer
    #25

---
+++ projects/moq/src/lib/type-explorers/expression.has-method.explorer.ts
@@ -0,0 +1,7 @@
+import { ExpectedExpressions } from "../expected-expressions/expected-expressions";
+
+export class ExpressionHasMethodExplorer {
+    public has(name: PropertyKey, expression: ExpectedExpressions<unknown>): boolean {
+        throw new Error("Not Implemented");
+    }
+}
---
+++ projects/moq/src/lib/type-explorers/object.has-method.explorer.ts
@@ -0,0 +1,5 @@
+export class ObjectHasMethodExplorer {
+    public has(name: PropertyKey, object: unknown): boolean {
+        throw new Error("Not Implemented");
+    }
+}
