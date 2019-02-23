ee27574ac959c774898d39b3f360611aaf78fb68
dvabuzyarov <dvabuzyarov@gmail.com>
2019-02-23 15:25:12 +0200

    fix(docs): publish docs
    #25

---
+++ projects/releaser/src/lib/releaser.ts
@@ -0,0 +1,19 @@
+import * as semanticRelease from "semantic-release";
+import { createReleaseMarker } from "./create-release-marker";
+
+export async function release() {
+    try {
+        const result = await semanticRelease({
+            branch: "master",
+            repositoryUrl: "https://github.com/dvabuzyarov/moq.ts",
+            pkgRoot: "dist/moq"
+        });
+        if (result) {
+            await createReleaseMarker();
+        }
+    } catch (err) {
+        console.error("The automated release failed with %O", err);
+    }
+}
+
+release();
---
+++ projects/releaser/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of releaser.ts
+ */
+
+export * from "./lib/releaser";
+
+
